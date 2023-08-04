71a14e7d742b2d22a35fcc5c68201c0aa2530b5c
newdevorder <139909380+newdevorder@users.noreply.github.com>
2023-08-04 12:55:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,42 +12,42 @@ st.title('Welcome To Streamlit Quickstart!')
 
 st.divider()
 
-st.subheader('Hello World - Part One')
+st.subheader('1. Text')
 st.write('Hello, World!')
 code = '''st.write('Hello, World!')'''
 st.code(code, language='python')
 
 st.divider()
 
-st.subheader('Hello World - Part Two')
+st.subheader('2. Text With Italics')
 st.write('Hello, *World!*')
 code = '''st.write('Hello, *World!*')'''
 st.code(code, language='python')
 
 st.divider()
 
-st.subheader('Hello World - Part Three')
+st.subheader('3. Text With Emojis')
 st.write('Hello, *World!* :sunglasses:')
 code = '''st.write('Hello, *World!* :sunglasses:')'''
 st.code(code, language='python')
 
 st.divider()
 
-st.subheader('Hello World - Part Four')
+st.subheader('4. Text With Bolding')
 st.markdown('Streamlit is **_really_ cool**.')
 code = '''st.markdown('Streamlit is **_really_ cool**.')'''
 st.code(code, language='python')
 
 st.divider()
 
-st.subheader('Hello World - Part Five')
+st.subheader('5. Text With Color')
 st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
 code = '''st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")'''
 st.code(code, language='python')
 
 st.divider()
 
-st.subheader('Hello World - Part Six')
+st.subheader('6. Text With Equations')
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
 code = '''st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")'''
 st.code(code, language='python')
