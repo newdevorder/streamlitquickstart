871d1d28a087e323aaba0d3edbefcddb675637c0
newdevorder <139909380+newdevorder@users.noreply.github.com>
2023-08-04 12:43:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 
+
+code = '''st.write('Hello, World!')'''
+
 st.write('Hello, World!')
+st.code(code, language='python')
 
 
 st.write('Hello, *World!*')
