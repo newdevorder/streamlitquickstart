fe783051a3195f8765ef7d31bdbe70c7cd69aeb2
newdevorder <139909380+newdevorder@users.noreply.github.com>
2023-08-04 12:49:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,26 @@ st.code(code, language='python')
 st.write('Hello, *World!* :sunglasses:')
 code = '''st.write('Hello, *World!* :sunglasses:')'''
 st.code(code, language='python')
+
+st.markdown('Streamlit is **_really_ cool**.')
+code = '''st.markdown('Streamlit is **_really_ cool**.')'''
+st.code(code, language='python')
+
+
+st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
+code = '''st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")'''
+st.code(code, language='python')
+
+st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+code = '''st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")'''
+st.code(code, language='python')
+
+
+
+
+
+
+
+
+
+
